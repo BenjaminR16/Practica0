e781b4fe86dc4025637b8e4a0d9fd42297c3b6b9
Benjamin Rocha <brcj16@gmail.com>
2024-11-25 12:24:58 +0100

    tercer commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,21 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Lottie
+    implementation("com.airbnb.android:lottie:6.1.0")
+
+    //implementacion del swipe
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    // CircleImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
--- app/src/main/java/com/ben/climatecrash/Splash.java
+++ app/src/main/java/com/ben/climatecrash/Splash.java
@@ -1,6 +1,7 @@
 package com.ben.climatecrash;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 public class Splash extends AppCompatActivity {
+
+    private ImageView fondo;
+    private LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +24,16 @@ public class Splash extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
 
+        // Configurar la imagen de fondo con Glide
+        fondo = findViewById(R.id.fondo);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1587513868965-0c8f1d6d1f3f") // URL de ejemplo
+                .transition(DrawableTransitionOptions.withCrossFade(600))
+                .centerCrop()
+                .into(fondo);
 
 
+        // Ajustar los insets para adaptarse al sistema (barras de estado y navegación)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
